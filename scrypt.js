4c6a24b648fa2a15fccb3b62dcee834f0e27bc94
lashasaata <lashasaata@gmail.com>
2024-01-29 07:03:40 -0800

    add completely changed js code because it only worked on the divs

--- scrypt.js
+++ scrypt.js
@@ -1,32 +1,57 @@
 const divs = document.getElementsByClassName("questionBox");
-const divsArray = Array.from(divs);
-let divIndex = 5;
+let divsArray = Array.from(divs);
+const headers = document.querySelectorAll("h3");
+let headersArray = Array.from(headers);
+const arrows = document.getElementsByClassName("arrow");
+let arrowsArray = Array.from(arrows);
+const paragraps = document.querySelectorAll("p");
+let paragrapsArray = Array.from(paragraps);
+// index is 5 at the start-end, because we dont want index and activeIndex would be same to close div;
+let index = 5;
+
 divsArray.forEach((element) => {
   element.addEventListener("click", (event) => {
-    let exactDiv = event.target;
-    console.log(divsArray.indexOf(element));
-
-    if (divIndex !== divsArray.indexOf(element)) {
-      exactDiv.childNodes[1].style.color = "#1E1F36";
-      exactDiv.childNodes[1].style.fontWeight = "700";
-      exactDiv.childNodes[3].style.transform = "rotate(180deg)";
-      exactDiv.childNodes[5].style.display = "block";
-      exactDiv.style.height = "90px";
-      if (divIndex >= 0 && divIndex <= 4) {
-        divsArray[divIndex].childNodes[1].style.color = "#4b4c5f";
-        divsArray[divIndex].childNodes[1].style.fontWeight = "400";
-        divsArray[divIndex].childNodes[3].style.transform = "rotate(0deg)";
-        divsArray[divIndex].childNodes[5].style.display = "none";
-        divsArray[divIndex].style.height = "35px";
-      }
-      divIndex = divsArray.indexOf(element);
-    } else {
-      exactDiv.childNodes[1].style.color = "#4b4c5f";
-      exactDiv.childNodes[1].style.fontWeight = "400";
-      exactDiv.childNodes[3].style.transform = "rotate(0deg)";
-      exactDiv.childNodes[5].style.display = "none";
-      exactDiv.style.height = "35px";
-      divIndex = 5;
-    }
+    let exactElement = event.target;
+    let activeIndex = headersArray.indexOf(exactElement);
+    showInfo(activeIndex);
   });
 });
+arrowsArray.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    let exactElement = event.target;
+    let activeIndex = arrowsArray.indexOf(exactElement);
+    showInfo(activeIndex);
+  });
+});
+divsArray.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    let exactElement = event.target;
+    let activeIndex = divsArray.indexOf(exactElement);
+    showInfo(activeIndex);
+  });
+});
+
+function showInfo(activeIndex) {
+  if (index !== activeIndex) {
+    headersArray[activeIndex].style.color = "#1E1F36";
+    headersArray[activeIndex].style.fontWeight = "700";
+    arrowsArray[activeIndex].style.transform = "rotate(180deg)";
+    paragrapsArray[activeIndex].style.display = "block";
+    divsArray[activeIndex].style.height = "90px";
+    if (index >= 0 && index <= 4) {
+      headersArray[index].style.color = "#4b4c5f";
+      headersArray[index].style.fontWeight = "400";
+      arrowsArray[index].style.transform = "rotate(0deg)";
+      paragrapsArray[index].style.display = "none";
+      divsArray[index].style.height = "35px";
+    }
+    index = activeIndex;
+  } else {
+    headersArray[activeIndex].style.color = "#4b4c5f";
+    headersArray[activeIndex].style.fontWeight = "400";
+    arrowsArray[activeIndex].style.transform = "rotate(0deg)";
+    paragrapsArray[activeIndex].style.display = "none";
+    divsArray[activeIndex].style.height = "35px";
+    index = 5;
+  }
+}
